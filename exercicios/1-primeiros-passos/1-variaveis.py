63601764349f60cfe327ad406435920dc38965c0
RafaellTrevizann <rafaelltrevizann@hotmail.com>
2024-10-29 20:11:51 -0300

    Update 1-variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Rafael"
+idade = "28"
 # Declare variáveis de cada tipo numérico
-
-
+salário = "10.000"
 # Declare uma variável do tipo booleano
+vddeiro = "True"
+falso = "False"
